80a27c8a2e6e9d8dda0a43026ec9614512d76efa
varshini <dreamvarshini@gmail.com>
2021-09-04 11:14:46 +0530

    ADDED: myhashtable class to implement hashtable

---
+++ src/com/bridgelabz/datastructures/MyHashTable.java
@@ -0,0 +1,84 @@
+package com.bridgelabz.datastructures;
+
+import java.util.ArrayList;
+
+import com.bridgelabz.datastructures.MyLinkedList;
+
+public class MyHashTable<K,V> 
+{
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
+	
+	public MyHashTable(int numBuckets) 
+	
+	{
+		this.numBuckets=numBuckets;
+		this.myBucketArray=new ArrayList<>(numBuckets);
+		
+		for(int index=0;index<numBuckets;index++)
+		{
+			
+			this.myBucketArray.add(null);
+		}
+	}
+	
+	public V get(K key)
+	{
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if(myLinkedList==null) return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+		return (myMapNode==null)? null : myMapNode.getValue();
+		
+	}
+	
+	public void add(K key,V value)
+	{
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList=this.myBucketArray.get(index);
+		if(myLinkedList==null)
+		{
+			myLinkedList = new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedList);
+		}
+		
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+		if(myMapNode==null)
+		{
+			myMapNode= new MyMapNode<>(key,value);
+			myLinkedList.appendNode(myMapNode);
+			
+		}
+		else
+		{
+			myMapNode.setValue(value);
+		}
+		
+	}
+	
+	public void remove(K key)
+	{
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList=this.myBucketArray.get(index);
+		myLinkedList.deleteGivenKeyNode(key);
+		
+		
+	}
+	
+	public String toString()
+	{
+		return "MyLinkedHashMap List{" + myBucketArray +'}';
+	}
+
+	private int getBucketIndex(K key) 
+	
+	{
+		int hashCode= Math.abs(key.hashCode());
+		int index=hashCode%numBuckets;
+		return index;
+	}
+	
+	
+	
+
+}
