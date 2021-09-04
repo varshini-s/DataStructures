56d9d26fcd064bb3ce9ba4029861744c64b41918
varshini <dreamvarshini@gmail.com>
2021-09-04 11:07:49 +0530

    ADDED: Stack class to implement stack data structure

---
+++ src/com/bridgelabz/datastructures/MyStack.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.datastructures;
+
+import com.bridgelabz.datastructures.*;
+
+public class MyStack<K> 
+
+{
+	private final MyLinkedList myLinkedList;
+	
+	public MyStack() 
+	{
+		this.myLinkedList = new MyLinkedList();
+	}
+	
+	public void push(INodeIF<K> newNode)
+	{
+		myLinkedList.appendNode(newNode);
+		
+	}
+	
+	public void printStack()
+	{
+		myLinkedList.printMyNodes();
+	}
+	
+	public INodeIF<K> peek()
+	{
+		return myLinkedList.head;
+	}
+	
+	public INodeIF<K> pop()
+	{
+		return myLinkedList.popLast();
+	}
+	
+	public boolean isEmpty()
+	{
+		return myLinkedList.isEmpty();
+	}
+	
+	public int size()
+	{
+		return myLinkedList.size();
+	}
+	public String toString()
+	{
+		return myLinkedList.toString();
+		
+	}
+	
+}
