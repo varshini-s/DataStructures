fc9772b896ec9cc7cdb3276e206196c59344a581
varshini <dreamvarshini@gmail.com>
2021-09-04 10:06:41 +0530

    ADDED:class created for linkedlist

---
+++ src/com/bridgelabz/datastructures/MyLinkedList.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.datastructures;
+
+public class MyLinkedList<K>
+
+{
+	public INodeIF<K> head;
+	public INodeIF<K> tail;
+	
+	public MyLinkedList() 
+	
+	{
+		this.head=null;
+		this.tail=null;
+		
+	}
+	
+	
+	
+
+}
