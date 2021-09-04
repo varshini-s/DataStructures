28a4cafa23e6f7c2ba1ca985c7aaddebd9143778
varshini <dreamvarshini@gmail.com>
2021-09-04 11:05:31 +0530

    ADDED: my node class to implement Inode

---
+++ src/com/bridgelabz/datastructures/MyNode.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.datastructures;
+
+public class MyNode<K extends Comparable<K>> implements INodeIF<K>
+
+{
+	
+	private K key;
+	private INodeIF<K> next;
+
+	
+
+	public MyNode(K key)
+	{
+		
+		this.key=key;
+		this.next=null;
+	}
+	
+	public INodeIF<K> getNext() 
+	{
+		return next;
+	}
+
+	@Override
+	public void setNext(INodeIF<K> next) 
+	{
+		this.next = next;
+		
+	}
+
+	
+
+	@Override
+	public K getKey() 
+	{
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) 
+	{
+		this.key=key;
+	}
+	
+	public String toString()
+	{
+		StringBuilder myNodeString = new StringBuilder();
+		myNodeString.append("MyNode{"+"key=").append(key).append("}");
+		if(next!=null)
+			myNodeString.append("->").append(next);
+		return myNodeString.toString();
+		
+	}
+
+	
+
+}
