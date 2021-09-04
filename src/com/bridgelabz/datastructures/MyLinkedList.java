c73c6c14c5eb8deaf41570c64f3f07d215de59cc
varshini <dreamvarshini@gmail.com>
2021-09-04 10:36:15 +0530

    ADDED: appendNode function in linkedlist class

--- src/com/bridgelabz/datastructures/MyLinkedList.java
+++ src/com/bridgelabz/datastructures/MyLinkedList.java
@@ -32,6 +32,26 @@ public class MyLinkedList<K>
 		}
 		
 	}
+	public void appendNode(INodeIF<K> newNode)
+	{
+		if(this.head==null)
+		{
+			this.head=newNode;
+		}
+		if(this.tail==null)
+		{
+			this.tail=newNode;
+		}
+		else
+		{
+			
+			
+			this.tail.setNext(newNode);
+			this.tail=newNode;
+		}
+		
+		
+	}
 	
 	
 
