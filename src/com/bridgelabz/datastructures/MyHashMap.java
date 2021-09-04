37f31c0f34eef9675ebf2f990e0ae0c1424c2e6b
varshini <dreamvarshini@gmail.com>
2021-09-04 11:12:51 +0530

    ADDED: function to get an element from hashmap

--- src/com/bridgelabz/datastructures/MyHashMap.java
+++ src/com/bridgelabz/datastructures/MyHashMap.java
@@ -12,33 +12,12 @@ public class MyHashMap<K,V>
 	{
 		this.myLinkedList=new MyLinkedList<>();
 	}
-	
+
 	public V get(K key)
 	{
 		
 		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
 		return (myMapNode==null)? null : myMapNode.getValue();
-	}
-	
-	public void add(K key,V value) 
-	{
-		MyMapNode<K, V> newNode= (MyMapNode<K, V>) this.myLinkedList.search(key);
-		if(newNode==null)
-		{
-			newNode=new MyMapNode<>(key,value);
-			this.myLinkedList.appendNode(newNode);
-		}
-		else
-		{
-			newNode.setValue(value);
-		}
-		
-		
-	}
-	
-	public String toString()
-	{
-		return "MYHashMapNodes{"+ myLinkedList+'}';
 	}
 
 }
